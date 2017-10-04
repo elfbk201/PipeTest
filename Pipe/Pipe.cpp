a8ac40da59ffb4b19861e16cc74737bb91c5ab04
Alexandr Krushev <apokalipsis_arma@hotmail.com>
2017-10-04 23:31:24 +0300

    Finish thi

--- Pipe/Pipe.cpp
+++ Pipe/Pipe.cpp
@@ -59,13 +59,13 @@ std::string Pipe::ReadPipe(std::string name)
 	saAttr.bInheritHandle = TRUE;
 	saAttr.lpSecurityDescriptor = NULL;
 	if (!CreatePipe(&g_hChildStd_OUT_Rd, &g_hChildStd_OUT_Wr, &saAttr, 0))
-		ErrorExit(TEXT("StdoutRd CreatePipe"));
+		return "";
 	if (!SetHandleInformation(g_hChildStd_OUT_Rd, HANDLE_FLAG_INHERIT, 0))
-		ErrorExit(TEXT("Stdout SetHandleInformation"));
+		return "";
 	if (!CreatePipe(&g_hChildStd_IN_Rd, &g_hChildStd_IN_Wr, &saAttr, 0))
-		ErrorExit(TEXT("Stdin CreatePipe"));
+		return "";
 	if (!SetHandleInformation(g_hChildStd_IN_Wr, HANDLE_FLAG_INHERIT, 0))
-		ErrorExit(TEXT("Stdin SetHandleInformation"));
+		return "";
 
 	TCHAR szCmdline[] = TEXT("child");
 	PROCESS_INFORMATION piProcInfo;
@@ -93,14 +93,14 @@ std::string Pipe::ReadPipe(std::string name)
 
 					   // If an error occurs, exit the application. 
 	if (!bSuccess)
-		ErrorExit(TEXT("CreateProcess"));
+		return "";
 	else
 	{
 		CloseHandle(piProcInfo.hProcess);
 		CloseHandle(piProcInfo.hThread);
 	}
 
-	Sleep(2000);
+	//Sleep(2000);
 
 	DWORD dwRead;
 	CHAR chBuf[BUFSIZE];
@@ -120,33 +120,3 @@ std::string Pipe::ReadPipe(std::string name)
 	return str;
 }
 
-
-
-void ErrorExit(PTSTR lpszFunction)
-{
-	LPVOID lpMsgBuf;
-	LPVOID lpDisplayBuf;
-	DWORD dw = GetLastError();
-
-	FormatMessage(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER |
-		FORMAT_MESSAGE_FROM_SYSTEM |
-		FORMAT_MESSAGE_IGNORE_INSERTS,
-		NULL,
-		dw,
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-		(LPTSTR)&lpMsgBuf,
-		0, NULL);
-
-	lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT,
-		(lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)lpszFunction) + 40)*sizeof(TCHAR));
-	StringCchPrintf((LPTSTR)lpDisplayBuf,
-		LocalSize(lpDisplayBuf) / sizeof(TCHAR),
-		TEXT("%s failed with error %d: %s"),
-		lpszFunction, dw, lpMsgBuf);
-	MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK);
-
-	LocalFree(lpMsgBuf);
-	LocalFree(lpDisplayBuf);
-	ExitProcess(1);
-}
